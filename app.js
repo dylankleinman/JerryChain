8a0dff21270646d77f490ae9eb97de2fe39c9b04
Dylan Kleinman <DKleinman@compusearch.com>
2021-03-05 10:22:30 -0700

    trying again

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+// const express = require('express')
+// const app = express()
+// // const port = 3500
+
+// const port = process.env.PORT || 3000  //port equals port or 3000
+
+// app.listen(port, console.log('listening at port '+port))
+// app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
 const express = require('express')
 const app = express()
-// const port = 3500
-
 const port = process.env.PORT || 3000  //port equals port or 3000
 
-app.listen(port, console.log('listening at port '+port))
+// app.listen(port, console.log('listening at port '+port))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
