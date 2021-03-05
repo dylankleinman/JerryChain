0afea5bd9b5be4818a3fc6df501ddbc936b025c9
Dylan Kleinman <DKleinman@compusearch.com>
2021-03-05 10:17:01 -0700

    port issues

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+// const port = 3500
+
+const port = process.env.PORT || 3000  //port equals port or 3000
 
 app.listen(port, console.log('listening at port '+port))
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
